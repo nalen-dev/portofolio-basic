fa07f2b83cb06091a153d37a87c8d172caaf5375
nalendev <nalendradev@gmail.com>
2025-06-01 13:21:42 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Typed from "typed.js";
+
+const myType =  new Typed("#whoami",{
+    strings:["Fullstack Developer","Low Level Enthusiast", "Forever Learner"],
+    loop:true,
+    typeSpeed:80,
+    showCursor:false
+})
